c6407b53419498f3d90068e818546d565d8da58f
Александр <kyplinov@gmail.com>
2019-11-12 14:49:53 +0300

    1

--- js/game.js
+++ js/game.js
@@ -1,8 +1,18 @@
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("game"); //Выбор canvas
+const ctx = canvas.getContext("2d"); //Определение типа игры
 
-const ground = new Image();
-ground.scr = "img/ground.png";
+const ground = new Image(); //Создание объекта поле игры
+ground.scr = "img/ground.png"; //Путь к изображению
 
-const food = new Image();
-food.scr = "img/food.png";
+const food = new Image(); //Создание объекта еда
+food.scr = "img/donut.png"; // Путь к изображению
+
+let box = 32;
+
+let score = 0;
+
+function drawGame() {
+    ctx.drawImage(ground, 0, 0);
+}
+
+let game = setInterval(drawGame, 100);
